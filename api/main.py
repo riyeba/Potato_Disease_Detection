eb020c592bf894b9edfba19a3f8bf64beaa65bc8
Akinpennu Taiwo <moriyebas@gmail.com>
2025-04-10 13:31:24 +0300

    april10

--- api/main.py
+++ api/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_origins=["http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
-    # allow_headers=["*"],
+    allow_headers=["*"],
 )
 
 
